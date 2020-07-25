4dd18a65ae8a9d33dcd8de402e36f185a8df85e0
Arogya <kharelarogya@hotmail.com>
2020-07-25 13:36:46 +0900

    Initial commit

---
+++ Assets/Scipts/Planets/CelestialBody.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Rigidbody))]
+public class CelestialBody : MonoBehaviour
+{
+    public float radius;
+    public float surfaceGravity;
+    public Vector3 initialVelocity;
+    public string bodyName = "UNNAMED";
+    Transform _meshHolder; 
+    public Vector3 Velocity { get; private set; }
+    public float Mass { get; private set; }
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.mass = Mass;
+        Velocity = initialVelocity;
+    }
+
+    public void UpdateVelocity(CelestialBody[] allBodies, float timeStep)
+    {
+        foreach (var otherBody in allBodies)
+        {
+            if (otherBody != this)
+            {
+                Vector3 otherPosition = otherBody._rb.position;
+                Vector3 mPosition = _rb.position;
+
+                float sqrDst = (otherPosition - mPosition).sqrMagnitude;
+                Vector3 forceDir = (otherPosition - mPosition).normalized;
+
+                // F = Gm1m2/r^2; F = m1a, a = F/m1; a = F/m1 * Gm1m2/r^2 = FGm2/r^2 
+                Vector3 acceleration = forceDir * Universe.GravitationalConstant * otherBody.Mass / sqrDst;
+                Velocity += acceleration * timeStep;
+            }
+        }
+    }
+
+    public void UpdateVelocity(Vector3 acceleration, float timeStep)
+    {
+        Velocity += acceleration * timeStep;
+    }
+
+    public void UpdatePosition(float timeStep)
+    {
+        _rb.MovePosition(_rb.position + Velocity * timeStep);
+    }
+
+    private void OnValidate()
+    {
+        Mass = surfaceGravity * radius * radius / Universe.GravitationalConstant;
+        _meshHolder = transform.GetChild(0);
+        _meshHolder.localScale = Vector3.one * radius;
+        gameObject.name = bodyName;
+    }
+
+
+    public Rigidbody Rigidbody
+    {
+        get { return _rb; }
+    }
+
+    public Vector3 Position
+    {
+        get { return _rb.position; }
+    }
+}
---
+++ Assets/Scipts/Planets/NBodySimulation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using Object = System.Object;
+
+public class NBodySimulation : MonoBehaviour
+{
+    private CelestialBody[] _bodies;
+    private static NBodySimulation _instance;
+
+    void Awake()
+    {
+        _bodies = FindObjectsOfType<CelestialBody>();
+        Time.fixedDeltaTime = Universe.PhysicsTimeStep;
+        Debug.Log("Setting fixedDeltaTime to: " + Universe.PhysicsTimeStep);
+    }
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i < _bodies.Length; i++)
+        {
+            Vector3 acceleration = CalculateAcceleration(_bodies[i].Position, _bodies[i]);
+            _bodies[i].UpdateVelocity(acceleration, Universe.PhysicsTimeStep);
+        }
+
+        for (int i = 0; i < _bodies.Length; i++)
+        {
+            _bodies[i].UpdatePosition(Universe.PhysicsTimeStep);
+        }
+    }
+
+    public static Vector3 CalculateAcceleration(Vector3 point, CelestialBody ignoreBody = null)
+    {
+        Vector3 acceleration = Vector3.zero;
+        foreach (var body in Instance._bodies)
+        {
+            if (body != ignoreBody)
+            {
+                Vector3 bodyPosition = body.Position;
+
+                float sqrDst = (bodyPosition - point).sqrMagnitude;
+                Vector3 forceDir = (bodyPosition - point).normalized;
+                acceleration += Universe.GravitationalConstant * body.Mass * forceDir / sqrDst;
+            }
+        }
+
+        return acceleration;
+    }
+
+    public CelestialBody[] Bodies => Instance._bodies;
+
+    static NBodySimulation Instance
+    {
+        get
+        {
+            if (!ReferenceEquals(_instance, null))
+            {
+                _instance = FindObjectOfType<NBodySimulation>();
+            }
+
+            return _instance;
+        }
+    }
+}
